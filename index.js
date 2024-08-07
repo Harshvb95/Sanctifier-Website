aeb5cbde3ea90e4bb40f7463c8c676f50e8d3def
Harshavardhan Bhattad <119807300+Harshvb95@users.noreply.github.com>
2024-08-07 12:58:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+function sendMail() {
+    var params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value,
+    };
+  
+    const serviceID = "service_3l6i0t3";
+    const templateID = "template_eyjkt8b";
+  
+      emailjs.send(serviceID, templateID, params)
+      .then(res=>{
+          document.getElementById("name").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("message").value = "";
+          console.log(res);
+          alert("Your message sent successfully!!")
+  
+      })
+      .catch(err=>console.log(err));
+  
+  }
